776ba18d2caf70f5b160e997c80f5555bd80dea8
Joshua Byrd <phocks@gmail.com>
2017-06-22 16:35:23 +1000

    Cycle colors on measure change

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = html`<div class="Census-100"></div>`;
 container.replaceChild(root, placeholder);
 
 // Set ABC color scale. Matches measure names with colors
-const color = scale.scaleOrdinal(['#3C6998', '#B05154', '#1B7A7D', '#8D4579', '#97593F','#605487', '#306C3F'])
+const color = scale.scaleOrdinal(['#3C6998', '#B05154', '#1B7A7D', '#8D4579', '#97593F', '#605487', '#306C3F'])
                 //    .domain([0, 1, 2, 3, 4, 5, 6]);  // Using measure names now as domain
 let currentColor = 'none';
 
@@ -80,6 +80,22 @@ function update(e) {
     // Set color according to measure
     currentColor = (e) ? e.detail.closestMark.el.dataset.measure : currentColor;
     rootSelection.style('background-color', color(currentColor));
+
+    function hexToRgbA(hex){
+        let c;
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            c = hex.substring(1).split('');
+            if(c.length== 3){
+                c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+            }
+            c = '0x'+c.join('');
+            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.5)';
+        }
+        throw new Error('Bad Hex');
+    }
+
+    d3.selectAll('.Scrollyteller-content')
+        .style('background-color', hexToRgbA(color(currentColor)));
 
     console.time('event');
 
