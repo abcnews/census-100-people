e32e41c1032fd2cf4c829f95f3c10d41828d3eee
Joshua Byrd <phocks@gmail.com>
2017-06-22 16:52:03 +1000

    Using alternate color values breaks paragraph width oops

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ container.replaceChild(root, placeholder);
 // Set ABC color scale. Matches measure names with colors
 const color = scale.scaleOrdinal(['#3C6998', '#B05154', '#1B7A7D', '#8D4579', '#97593F', '#605487', '#306C3F'])
                 //    .domain([0, 1, 2, 3, 4, 5, 6]);  // Using measure names now as domain
+const paragraphColor = scale.scaleOrdinal(['#29517C', '#8A4042', '#135E60', '#703660', '#71412D', '#483F67', '#22512E']);
 let currentColor = 'none';
 
 const margin = 10;
@@ -81,7 +82,7 @@ function update(e) {
     currentColor = (e) ? e.detail.closestMark.el.dataset.measure : currentColor;
     rootSelection.style('background-color', color(currentColor));
 
-    function hexToRgbA(hex){
+    function hexToRgbA(hex){ // also adds half alpha
         let c;
         if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
             c = hex.substring(1).split('');
@@ -89,7 +90,7 @@ function update(e) {
                 c= [c[0], c[0], c[1], c[1], c[2], c[2]];
             }
             c = '0x'+c.join('');
-            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.5)';
+            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.9)';
         }
         throw new Error('Bad Hex');
     }
