763fa1c5a0838710f42bbfdbeb5f2b00dc03bfb9
Joshua Byrd <phocks@gmail.com>
2017-06-27 12:58:37 +1000

    Polyfil for IE

--- src/index.js
+++ src/index.js
@@ -265,7 +265,7 @@ function init(){
             update();
         });
         update();
-    }, 200);
+    }, 300);
 }
 
 function getRandomIntInclusive(min, max) {
@@ -287,11 +287,38 @@ function hexToRgbA(hex){ // also adds alpha
         throw new Error('Bad Hex');
     }
 
+// Polyfil for IE etc
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
+
 // Stops dots clogging up the system on reload
 jankdefer(init, {
     timeout: 5000,
     threshold: 10,
-    debug: true
+    debug: false
 });
 
 // Because reloading breaks colors and sections maybe scroll to top on reload
